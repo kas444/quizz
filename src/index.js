b6b2cfcfbb3a3041e1a21ec73ecc68e9245efb8d
kas444 <kasia.czerwinska06@gmail.com>
2022-06-25 23:29:52 +0200

    2 files modified

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
 
 import App from './App';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );

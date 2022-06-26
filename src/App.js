cbcdb35a4aeb1e420e6857ed9f240fcfc6442e4d
Katarzyna Czerwinska <katarzyna.czerwinska@stepstone.com>
2022-06-26 09:45:30 +0200

    added gitignore

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { QuestionView } from './views/question';
 export default function App() {
   return (
     <div>
-      <h1>Quizz</h1>
+      <h1>Quizz z wiedzy o prawie</h1>
       <QuestionView />
     </div>
   );
